dbf96d4a007e5bc3fb2d15bfec1fb0e4f19301e5
Arya-Inamdar07 <62196694+Arya-Inamdar07@users.noreply.github.com>
2024-11-27 18:54:08 +0530

    Add files via upload

---
+++ 1010101.py
@@ -0,0 +1,11 @@
+str ='1'
+for i in range (7,0,-2):
+    for j in range (i):
+        print (str,end='')
+        if str == '1':
+            str = '0'
+        else : 
+            str='1'
+
+    print ('')
+    str = '1'
---
+++ area of reactangle.py
@@ -0,0 +1,5 @@
+length = float(input("Enter length of Rectangle:"))
+breadth = float (input ("Enter breaadth of Rectangle:2"))
+area = length * breadth 
+print ("Area of rectsngle is : ",area)
+ 
